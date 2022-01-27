9b687ed8e54f98600ce36ec105fabe4744bf1fde
devcarlan <devcarlan@gmail.com>
2022-01-26 21:15:28 -0500

    adding proptype checking to label

--- src/components/Label.jsx
+++ src/components/Label.jsx
@@ -1,9 +1,14 @@
+import PropTypes from 'prop-types'
 import '../styles.css'
 
-function Label( { text }) {
+function Label({ text }) {
   return (
     <p className='color-text' onClick={() => {navigator.clipboard.writeText(text)}} style={{textAlign: 'center', backgroundColor: '#fff', padding: '.5em', margin: '0'}}>{text}</p>
   )
+}
+
+Label.propTypes = {
+  text: PropTypes.string
 }
 
 export default Label
