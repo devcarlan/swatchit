0a948770acaaeb21d4d670c54626e672b3eb0c0f
devcarlan <devcarlan@gmail.com>
2022-02-24 23:03:05 -0500

    blocks now start with random colors

--- src/components/Block.jsx
+++ src/components/Block.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react';
 import Label from './Label'
 import '../styles.css'
 
-function Block() {
-  const [color, setColor] = useState({bgColor: '#fff',})
-  const [clicked, setClicked] = useState('Click Me!')
 
-  // generates random color hex
-  const randColor = () => {
-    return '#' +  Math.floor(Math.random()*16777215).toString(16)
-  }
+// generates random color hex
+const randColor = () => {
+  return '#' +  Math.floor(Math.random()*16777215).toString(16)
+}
+
+function Block() {
+  const [color, setColor] = useState({bgColor: randColor(),})
+  const [clicked, setClicked] = useState('Click Me!')
 
   // sets bgColor state of color to a random hexadecimal number
   // sets clicked state to empty string
