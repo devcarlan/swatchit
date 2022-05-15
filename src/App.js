68060e6e135bea61b7db04c3a7d32fe07de14682
devcarlan <devcarlan@gmail.com>
2022-05-14 21:04:38 -0400

    SwatchIt

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import './App.css';
+import './App.css'
 import Swatch from './components/Swatch'
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <div className='text-wrap'>
-      <h1>Swatch It!</h1>
-      <p>Click on a tile to change it's color.</p>
-      <p>Click on the label above a tile to copy it's hexadecimal value.</p>
+        <h1>SwatchIt!</h1>
+        <p>Click on a tile to change it's color.</p>
+        <p>Click on the label above a tile to copy it's hexadecimal value.</p>
       </div>
       <div className='swatch-wrap'>
         <Swatch />
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
