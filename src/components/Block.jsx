dcb77b7630f21846ebac5406d0f2f74a06234746
devcarlan <devcarlan@gmail.com>
2022-05-17 22:20:05 -0400

    adding better logic for dispalying 'click me' cta

--- src/components/Block.jsx
+++ src/components/Block.jsx
@@ -1,16 +1,15 @@
-import { useState } from 'react';
+import { useState } from 'react'
 import Label from './Label'
 import '../styles.css'
 
-
 // generates random color hex
 const randColor = () => {
-  return "#000000".replace(/0/g,() => (~~(Math.random()*16)).toString(16))
+  return '#000000'.replace(/0/g, () => (~~(Math.random() * 16)).toString(16))
 }
 
 function Block() {
-  const [color, setColor] = useState({bgColor: randColor(),})
-  const [clicked, setClicked] = useState('Click Me!')
+  const [color, setColor] = useState({ bgColor: randColor() })
+  const [clicked, setClicked] = useState(false)
 
   // sets bgColor state of color to a random hexadecimal number
   // sets clicked state to empty string
@@ -18,14 +17,18 @@ function Block() {
     setColor({
       bgColor: randColor(),
     })
-    setClicked('')
+    setClicked(true)
   }
 
   return (
     <div className='block-wrapper'>
       <Label text={color.bgColor} />
-      <div className='block' onClick={() => handleClick()} style={{backgroundColor: color.bgColor}}>
-        <p style={{textAlign: 'center',}}>{clicked}</p>
+      <div
+        className='block'
+        onClick={() => handleClick()}
+        style={{ backgroundColor: color.bgColor }}
+      >
+        {clicked ? '' : <p style={{ textAlign: 'center' }}>Click Me!</p>}
       </div>
     </div>
   )
